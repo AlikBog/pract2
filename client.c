40408461121875270e06794ef4182f909738aa81
Roffl <127674546+AlikBog@users.noreply.github.com>
2025-06-01 22:23:29 +0300

    Update client.c

--- client.c
+++ client.c
@@ -8,25 +8,29 @@
 #include <arpa/inet.h>
 
 #define PORT 8080
-#define SERVER_IP "127.0.0.1"  
+#define SERVER_IP "127.0.0.1"  // Локальный адрес для тестирования
 
 int main(int argc, char* argv[]) {
+    // filling address struct
     struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address)); 
+    memset(&server_address, 0, sizeof(server_address));  // Обнуляем структуру
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
-
+    
+    // Преобразование IP-адреса
     if (inet_pton(AF_INET, SERVER_IP, &server_address.sin_addr) <= 0) {
         perror("Invalid address/ Address not supported");
         exit(EXIT_FAILURE);
     }
 
+    // socket creation
     int network_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (network_socket < 0) {
         perror("Failed to create socket...");
         exit(EXIT_FAILURE);
     }
 
+    // connecting to server
     if (connect(network_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
         perror("Failed to connect to server...");
         close(network_socket);
@@ -34,17 +38,16 @@ int main(int argc, char* argv[]) {
     }
 
     // sending information
-    char message[] = "Богосьян Алик Альбертович ККСО-26-24";
-    size_t bytes_sent = send(network_socket, message, strlen(message), 0);
-
+    char message[] = "Богосьян Алик Альбертович ККСО-26-24";  
+    ssize_t bytes_sent = send(network_socket, message, strlen(message), 0);
+    
     if (bytes_sent < 0) {
         perror("Failed to send message");
-    }
-    else {
+    } else {
         printf("Message sent successfully: %s\n", message);
     }
 
-    // close the socket
+    // closing socket
     close(network_socket);
 
     return EXIT_SUCCESS;
